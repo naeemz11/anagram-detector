62036c02c23942c9a578c9b73b83736bebc93222
your_name <your_email@example.com>
2020-10-29 18:14:14 -0400

    commit tree e806fdbc1842f1d081fb039aa21b4fa3bddeff37

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+
+    attr_accessor :word 
+
+    def initialize(word)
+        @word = word 
+    end 
+
+    def match(word_to_match)
+        word_to_match.select do |element|
+            (@word.split("").sort) == (element.split("").sort)
+        end 
+        #binding.pry
+    end 
+
+end 
+
+
+#habibi= Anagram.new("Habibi")
